af35e556e3110e0995eddea2864f95dca3dc0d8e
fz-ss <1795234295@qq.com>
2024-08-12 16:16:03 +0800

    first commit

---
+++ cfg.py
@@ -0,0 +1,20 @@
+input_dir = "srt_path"
+output_dir = "srt_path_out"
+input_lang = "zh-CN"
+output_lang = ["en"]
+input_srt_suffix = ".srt"
+output_srt_suffix = "_translated.srt"
+
+google_translate = "https://translate.google.com/m?"
+single_enter = "\n"
+# single_enter = ")("
+double_enter = "\n\n" #...
+# double_enter = "\n..." #...
+# double_enter = "\n" #...
+
+
+
+
+remote_api_timeout = 10
+translate_error = "Translation failed"
+time_to_sleep = 1
